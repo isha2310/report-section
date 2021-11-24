027a61fb69c100465f086914416eedf6d4da9df6
Isha Agarwal <isha.a1023@gmail.com>
2021-11-24 11:36:29 +0530

    Table added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     let arr2 = [{type: 'Total IVs', count: '157.4k'},{type: 'Total IVs', count: '157.4k'},{type: 'Total IVs', count: '157.4k'},{type: 'Total IVs', count: '157.4k'},{type: 'Total IVs', count: '157.4k'},{type: 'Total IVs', count: '157.4k'},{type: 'Total IVs', count: '157.4k'},{type: 'Total IVs', count: '157.4k'},{type: 'Total IVs', count: '157.4k'}]
     let arr3 = [{type: 'Total IVs', count: '157.4k'},{type: 'Total IVs', count: '157.4k'},{type: 'Total IVs', count: '157.4k'}]
 
+    let table_data = [['Brand', 'Qty', 'MRP', 'NMV', 'Retail'], ['Good Vibes', 5, 500, 654, 5463], ['Good Vibes', 5, 500, 654, 5463], ['Good Vibes', 5, 500, 654, 5463], ['Good Vibes', 5, 500, 654, 5463], ['Good Vibes', 5, 500, 654, 5463]]
+
     arr.forEach((ele) => {
         let div = document.createElement("div")
         div.classList.add("report_element", "report_element1")
@@ -39,7 +41,7 @@ $(document).ready(function () {
         div.append(p1,p2)
         $("#report_heading3_div").append(div)
     })
-    
+
     arr.forEach((ele) => {
         let div = document.createElement("div")
         div.classList.add("report_element", "report_element1")
@@ -61,6 +63,32 @@ $(document).ready(function () {
     $("#dropbtn3").on('click', function(){
         document.getElementById("myDropdown3").classList.toggle("showDropdown");
     })
+
+    //$(".report_brandwise_table")
+
+    table_data.forEach((element, index) => {
+        let row = document.createElement("tr")
+        if(index === 0){
+           // row.classList.add("report_table_header")
+           console.log(index)
+            element.forEach((data) => {
+                let header = document.createElement("TH")
+                header.classList.add("report_table-data", "report_table_header")
+                header.innerHTML = data
+                row.append(header)
+            })
+            $(".report_brandwise_table").append(row)
+        } else {
+            element.forEach((data) => {
+                let row_data = document.createElement("td")
+                row_data.classList.add("report_table-data")
+                row_data.innerHTML = data
+                row.append(row_data)
+            })
+            $(".report_brandwise_table").append(row)
+        }    
+    })
+
     window.onclick = function(event) {
         if (!event.target.matches('.dropbtn')) {
           var dropdowns = document.getElementsByClassName("dropdown-content");
