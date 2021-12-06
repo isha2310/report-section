463660242c17e360291b7b4f0861648b93bd7f22
Isha Agarwal <isha.a1023@gmail.com>
2021-12-06 16:14:45 +0530

    API integrated and UI updated

--- index.js
+++ index.js
@@ -1,53 +1,60 @@
 $(document).ready(function () {
 
-    let payload = {
-        token: "W2yMnwfgn5FGoNPZORN0xg",
-        "data": {
-            "user_funnel": {
-                "Total IVs": "1.08M",
-                "Total IV Users": "157.54K",
-                "ATCs": "161.33K",
-                "ATC Users": "50.05K",
-                "Orders": "7.34K",
-                "Purchasers": "6.42K"
-            },
-            "calculated_fields": {
-                "NMV/IV":"5.33",
-                "NMV/IV_Users":"36.63",
-                "Conversion Rate":"4.66%",
-                "NMV/ATC":"35.77",
-                "NMV/ATC_Users":"115.28",
-                "ASP": "169",
-                "AOV":"787",
-                "NMV/Purchasers":"900",
-                "Quantity/Order":"4.65"
-            },
-            "miscellaneous":{
-                "MRP":"8.35M", 
-                "Quantity": "34.12K", 
-                "Discount %":"30.81%"
-            },
-            "nmv_freebies": {
-                "NMV": "5.78M",
-                "Offer Led MRP (Freebies)": "26.19K",
-                "PB NMV": "1.21M",
-                "Sampling MRP (Freebies)": "9.00",
-                "TP Only NMV": "3.99M",
-                "Liquidation MRP (Freebies)": "938.72K",
-                "Retailer NMV": "96.41K",
-                "Total Freebie Qty": "6.04K"
-            }
-              }
-    };
+    // let payload = {
+    //     token: "W2yMnwfgn5FGoNPZORN0xg",
+    //     "data": {
+    //         "user_funnel": {
+    //             "Total IVs": "1.08M",
+    //             "Total IV Users": "157.54K",
+    //             "ATCs": "161.33K",
+    //             "ATC Users": "50.05K",
+    //             "Orders": "7.34K",
+    //             "Purchasers": "6.42K"
+    //         },
+    //         "calculated_fields": {
+    //             "NMV/IV":"5.33",
+    //             "NMV/IV_Users":"36.63",
+    //             "Conversion Rate":"4.66%",
+    //             "NMV/ATC":"35.77",
+    //             "NMV/ATC_Users":"115.28",
+    //             "ASP": "169",
+    //             "AOV":"787",
+    //             "NMV/Purchasers":"900",
+    //             "Quantity/Order":"4.65"
+    //         },
+    //         "miscellaneous":{
+    //             "MRP":"8.35M", 
+    //             "Quantity": "34.12K", 
+    //             "Discount %":"30.81%"
+    //         },
+    //         "nmv_freebies": {
+    //             "NMV": "5.78M",
+    //             "Offer Led MRP (Freebies)": "26.19K",
+    //             "PB NMV": "1.21M",
+    //             "Sampling MRP (Freebies)": "9.00",
+    //             "TP Only NMV": "3.99M",
+    //             "Liquidation MRP (Freebies)": "938.72K",
+    //             "Retailer NMV": "96.41K",
+    //             "Total Freebie Qty": "6.04K"
+    //         }
+    //           }
+    // };
+
+    let payload  
     
     $.ajax({
-      type: "POST",
-      url: "https://app.fakejson.com/q",
-      data: payload,
-      success: function(resp) {
-          console.log(resp)
+      type: "GET",
+      url: "http://34.132.95.222:8081/v1/getalldata",
+      crossDomain:true,
+          contentType:'application/json',
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+      success: function(res) {
+          console.log(res)
+          let resp = res.mainTable
             let arr_users = resp.user_funnel
-            let arr_fields = resp.calculated_fields
+            let arr_fields = resp.calculatedFields
             let arr_miscellaneous = resp.miscellaneous
             let arr_nmv = resp.nmv_freebies
             let arr0 = [
@@ -82,7 +89,6 @@ $(document).ready(function () {
               ];
 
               for (let key of Object.keys(arr_users)) {
-                //console.log(key + " -> " + p[key])
                 let div = document.createElement("div");
                 div.classList.add("report_element", "report_element1");
                 let p1 = document.createElement("p");
@@ -96,7 +102,6 @@ $(document).ready(function () {
             }
 
             for (let key of Object.keys(arr_fields)) {
-                //console.log(key + " -> " + p[key])
                 let div = document.createElement("div");
                 div.classList.add("report_element", "report_element2");
                 let p1 = document.createElement("p");
@@ -110,7 +115,6 @@ $(document).ready(function () {
             }
 
             for (let key of Object.keys(arr_miscellaneous)) {
-                //console.log(key + " -> " + p[key])
                 let div = document.createElement("div");
                 div.classList.add("report_element", "report_element3");
                 let p1 = document.createElement("p");
@@ -124,7 +128,6 @@ $(document).ready(function () {
             } 
             
             for (let key of Object.keys(arr_nmv)) {
-                //console.log(key + " -> " + p[key])
                 let div = document.createElement("div");
                 div.classList.add("report_element", "report_element1");
                 let p1 = document.createElement("p");
@@ -136,17 +139,77 @@ $(document).ready(function () {
                 div.append(p1, p2);
                 $("#report_heading4_div").append(div);
             }
+            // let table_data = [
+            //   ["Brand", "Qty", "MRP", "NMV", "Retail"],
+            //   ["Good Vibes", 5, 500, 654, 5463],
+            //   ["Good Vibes", 5, 500, 654, 5463],
+            //   ["Good Vibes", 5, 500, 654, 5463],
+            //   ["Good Vibes", 5, 500, 654, 5463],
+            //   ["Good Vibes", 5, 500, 654, 5463],
+            // ];
+            // table_data.forEach((element, index) => {
+            //   let row = document.createElement("tr");
+            //   if (index === 0) {
+            //     // row.classList.add("report_table_header")
+            //     console.log(index);
+            //     element.forEach((data) => {
+            //       let header = document.createElement("TH");
+            //       header.classList.add("report_table-data", "report_table_header");
+            //       header.innerHTML = data;
+            //       row.append(header);
+            //     });
+            //     $(".report_brandwise_table").append(row);
+            //   } else {
+            //     element.forEach((data) => {
+            //       let row_data = document.createElement("td");
+            //       row_data.classList.add("report_table-data");
+            //       row_data.innerHTML = data;
+            //       row.append(row_data);
+            //     });
+            //     $(".report_brandwise_table").append(row);
+            //   }
+            // });
+            let table_data = [
+              ["Brand", "Qty", "MRP", "NMV", "Retail", "Orders", "Coupons", "IV", "IV Users", "convrsn_rate", "ATC/IV Users", "NMV/IV", "NMV/User", "ATCs", "ATC_Users", "AOV", "ASP", "Dscount%", "Freebie_", "Freebie_Qty"]]
+              table_data.forEach((element, index) => {
+                let row = document.createElement("tr");
+                if (index === 0) {
+                  // row.classList.add("report_table_header")
+                  console.log(index, element);
+                  element.forEach((data) => {
+                    let header = document.createElement("TH");
+                    header.classList.add("report_table-data", "report_table_header", "table_data");
+                    header.innerHTML = data;
+                    if(data === 'Brand'){
+                      header.classList.add("table_brand")
+                    }
+                    row.append(header);
+                  });
+                  $(".report_brandwise_table").append(row);
+                }
+              });
+              let tab = res.brandWise
+              tab.forEach((element) => {
+                let row = document.createElement("tr");
+                table_data[0].forEach((data) => {
+                  let val = element[data] || 'N/A'
+                  let row_data = document.createElement("td");
+                  row_data.classList.add("report_table-data", "table_data");
+                  row_data.innerHTML = val;
+                  if(data === 'Brand'){
+                    row_data.classList.add("table_brand")
+                  }
+                  row.append(row_data);
+                })
+                $(".report_brandwise_table").append(row);
+              })
+        },
+        error:function(request,error){
+          console.log(request, error)
         }
     });
 
-  let table_data = [
-    ["Brand", "Qty", "MRP", "NMV", "Retail"],
-    ["Good Vibes", 5, 500, 654, 5463],
-    ["Good Vibes", 5, 500, 654, 5463],
-    ["Good Vibes", 5, 500, 654, 5463],
-    ["Good Vibes", 5, 500, 654, 5463],
-    ["Good Vibes", 5, 500, 654, 5463],
-  ];
+  
 
   $("#dropbtn1").on("click", function () {
     document.getElementById("myDropdown1").classList.toggle("showDropdown");
@@ -156,29 +219,6 @@ $(document).ready(function () {
   });
   $("#dropbtn3").on("click", function () {
     document.getElementById("myDropdown3").classList.toggle("showDropdown");
-  });
-
-  table_data.forEach((element, index) => {
-    let row = document.createElement("tr");
-    if (index === 0) {
-      // row.classList.add("report_table_header")
-      console.log(index);
-      element.forEach((data) => {
-        let header = document.createElement("TH");
-        header.classList.add("report_table-data", "report_table_header");
-        header.innerHTML = data;
-        row.append(header);
-      });
-      $(".report_brandwise_table").append(row);
-    } else {
-      element.forEach((data) => {
-        let row_data = document.createElement("td");
-        row_data.classList.add("report_table-data");
-        row_data.innerHTML = data;
-        row.append(row_data);
-      });
-      $(".report_brandwise_table").append(row);
-    }
   });
 
   window.onclick = function (event) {
